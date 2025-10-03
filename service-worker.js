4a0546019adc7d33c92a729f713a548b00f6c413
CoachJonas <tom.4rnas@protonmail.com>
2025-10-03 23:32:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker installiert.');
+self.addEventListener('install', (e)=>{ self.skipWaiting(); });
+self.addEventListener('activate', (e)=>{ return self.clients.claim(); });
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
