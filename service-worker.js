f48f013d58804c1b47cefa5d490b1c05f36c2208
CoachJonas <tom.4rnas@protonmail.com>
2025-10-04 09:09:15 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('coach-v13-5').then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.png'])));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open('coach-v13-5').then(cache => cache.addAll([
+    './', './index.html', './manifest.json', './icon.png'
+  ])));
 });
-self.addEventListener('fetch', e => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request); if (r) return r;
-    try { const resp = await fetch(e.request); const c = await caches.open('coach-v13-5'); c.put(e.request, resp.clone()); return resp; }
-    catch(e) { return new Response('', {status:504}); }
-  })());
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
