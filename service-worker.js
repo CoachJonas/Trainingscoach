066b3cd0e702f6e17f9f9abf33b2b39ffc083154
CoachJonas <tom.4rnas@protonmail.com>
2025-10-17 03:52:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('tc-13-7').then(c=>c.addAll(['./','./index.html','./settings.html','./manifest.json','./icon.png'])));
+  e.waitUntil(caches.open('tc-13-10').then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.png','./service-worker.js'])));
 });
 self.addEventListener('fetch',e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
